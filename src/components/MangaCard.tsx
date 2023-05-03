78a521b8e92b35961f87e9bc4ba53b957177d11a
Ianius <michaelpy023@gmail.com>
2023-05-03 17:38:31 -0500

    separated PageNavigation in its own component

--- src/components/MangaCard.tsx
+++ src/components/MangaCard.tsx
@@ -6,6 +6,8 @@ import { saveAs } from 'file-saver';
 import { HiDotsHorizontal } from 'react-icons/hi';
 import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
 import JSZip from "jszip";
+import { createPortal } from "react-dom";
+import PageNavigation from "./PageNavigation";
 
 interface Props {
     id: string;
@@ -195,74 +197,11 @@ const MangaCard = ({ id, title, coverUrl, description }: Props) => {
                     <div
                         className='flex flex-wrap justify-center items-center gap-4'
                     >
-                        {(() => {
-                            const PAGES_PADDING = 2;
-                            const MIN_WIDTH_AND_HEIGHT = "min-w-[2.5em] min-h-[2.5em]"
-
-                            interface PageSelectorProps {
-                                active?: boolean;
-                                page: number;
-                            }
-
-                            interface IconProps {
-                                Component: ComponentType<any>;
-                            }
-
-                            interface ArrowProps {
-                                Component: ComponentType<any>;
-                                onClick: () => void;
-                                active: boolean;
-                            }
-
-                            const PageSelector = ({ active = false, page }: PageSelectorProps) => {
-                                return (
-                                    <button
-                                        onClick={_ => trySetPage(page)}
-                                        className={`${MIN_WIDTH_AND_HEIGHT} rounded-md transition-all ${active ? "bg-orange-500" : ""} ${active ? "hover:bg-orange-700" : "hover:bg-zinc-700"}`}
-                                    >
-                                        <span>
-                                            {page + 1}
-                                        </span>
-                                    </button>
-                                );
-                            };
-
-                            const Icon = ({ Component: Icon }: IconProps) => <div className={`${MIN_WIDTH_AND_HEIGHT} flex items-center justify-center`}><Icon className='w-1/2 h-1/2'/></div>;
-
-                            const Arrow = ({ Component, onClick, active }: ArrowProps) => <button className={`hover:bg-zinc-700 rounded-full ${active ? "text-white" : "text-zinc-700"}`} onClick={onClick}><Icon Component={Component}/></button>;
-
-                            const results: ReactNode[] = [];
-
-                            results.push(
-                                <Arrow
-                                    onClick={() => trySetPage(page - 1)}
-                                    Component={BsArrowLeft}
-                                    active={page > 0}
-                                />
-                            );
-
-                            if (page > PAGES_PADDING) results.push(<PageSelector page={0}/>);
-                            if (page > PAGES_PADDING + 1) results.push(<Icon Component={HiDotsHorizontal}/>);
-
-                            for (let i = page - PAGES_PADDING; i <= page + PAGES_PADDING; i++) {
-                                if (i < 0) continue;
-                                if (i > totalPages - 1) break;
-                                results.push(<PageSelector active={page === i} page={i}/>)
-                            }
-
-                            if (page + PAGES_PADDING < totalPages - 2) results.push(<Icon Component={HiDotsHorizontal}/>);
-                            if (page + PAGES_PADDING < totalPages - 1) results.push(<PageSelector page={totalPages - 1}/>);
-
-                            results.push(
-                                <Arrow
-                                    onClick={() => trySetPage(page + 1)}
-                                    Component={BsArrowRight}
-                                    active={page < totalPages - 1}
-                                />
-                            );
-
-                            return results;
-                        })()}
+                        <PageNavigation
+                            page={page}
+                            totalPages={totalPages}
+                            setter={setPage}
+                        />
                     </div>
                 </>
             }
